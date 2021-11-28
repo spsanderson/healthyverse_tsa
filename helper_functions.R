359fc73ac86ff7f588814d2fd4b26f0778b735a3
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-11-28 11:26:38 -0500

    Update helper_functions.R

--- helper_functions.R
+++ helper_functions.R
@@ -1,5 +1,16 @@
-downloads_file_date <- fs::file_info("old_downloads.RDS") %>%
-    dplyr::pull(birth_time)
+dl_birth_datetime <- function(){
+    
+    file_info("cran_logs.csv") %>%
+        pull(birth_time)
+    
+} 
 
-max_cran_date <- readr::read_rds("old_downloads.RDS") %>%
-    max(date)
+max_cran_datetime <- read.csv("cran_logs.csv") %>% 
+    as_tibble() %>%
+    select(date, time) %>%
+    mutate(
+        date_time = paste0(date, " ", time) %>%
+            lubridate::ymd_hms()
+    ) %>%
+    pull(date_time) %>%
+    max()
