a707fcd5b29794948f29598e8f76e0b12579eb2d
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-11-28 10:41:22 -0500

    Update get_data_functions.R

--- get_data_functions.R
+++ get_data_functions.R
@@ -1,8 +1,13 @@
 get_cran_data <- function(){
     
     url    <- "https://github.com/spsanderson/package-downloads/blob/master/"
-    f_name <- "old_downloads.RDS"
+    f_name <- "old_downloads.rds"
     f_url  <- paste0(url, f_name)
-    utils::download.file(f_url, f_name)
+    utils::download.file(f_url, f_name, method = "curl")
+
+}
+
+load_cran_data <- function(){
+    
     
 }
