4c738d6fa29552c3bb8922a5a8a2d556df08d0c8
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-11-29 11:37:36 -0500

    Update get_data_functions.R

--- 00_scripts/get_data_functions.R
+++ 00_scripts/get_data_functions.R
@@ -7,9 +7,20 @@ get_cran_data <- function() {
   write_csv(data, "01_data/cran_logs.csv")
 }
 
+get_package_release_data <- function(){
+  url <- "https://raw.githubusercontent.com/spsanderson/package-downloads/master/"
+  f_name <- "pkg_release_tbl.rds"
+  f_url <- paste0(url, f_name)
+  data <- readRDS(url(f_url, method = "libcurl"))
+  data <- as_tibble(data)
+  write_csv(data, "01_data/pkg_release_tbl.csv")
+}
+
 csv_to_rds <- function(){
   data <- read.csv("01_data/cran_logs.csv")
+  pkg_data <- read.csv("01_data/pkg_release_tbl.csv")
   saveRDS(data, "01_data/cran_logs.rds")
+  saveRDS(pkg_data, "01_data/pkg_release_tbl.rds")
 }
 
 load_cran_data <- function() {
