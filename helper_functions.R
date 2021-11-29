62ec47f22b9dff8566696ebde53baadb44560202
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-11-28 21:13:00 -0500

    Update helper_functions.R

--- helper_functions.R
+++ helper_functions.R
@@ -1,16 +1,20 @@
-dl_birth_datetime <- function(){
-    
-    file_info("cran_logs.csv") %>%
-        pull(birth_time)
-    
-} 
+dl_birth_datetime <- function() {
+  file_info("cran_logs.csv") %>%
+    pull(birth_time)
+}
 
-max_cran_datetime <- read.csv("cran_logs.csv") %>% 
+max_cran_datetime <- function() {
+  read.csv("cran_logs.csv") %>%
     as_tibble() %>%
     select(date, time) %>%
     mutate(
-        date_time = paste0(date, " ", time) %>%
-            lubridate::ymd_hms()
+      date_time = paste0(date, " ", time) %>%
+        lubridate::ymd_hms()
     ) %>%
     pull(date_time) %>%
     max()
+}
+
+hours_since_cran_log_update <- function(){
+    round(as.numeric(difftime(dl_birth_datetime(), max_cran_datetime(), units = "hours")), 2)
+}
