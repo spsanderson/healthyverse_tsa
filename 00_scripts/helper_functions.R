bf9a85305430ddd1a73c6542378032f39ec0a05d
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-11-29 10:53:46 -0500

    Update helper_functions.R

--- 00_scripts/helper_functions.R
+++ 00_scripts/helper_functions.R
@@ -18,3 +18,11 @@ max_cran_datetime <- function() {
 hours_since_cran_log_update <- function(){
     round(as.numeric(difftime(dl_birth_datetime(), max_cran_datetime(), units = "hours")), 2)
 }
+
+update_log_message <- function(){
+  if(hours_since_cran_log_update() > 36){
+    print("Consider updating the cran log file from the package-downloads project.")
+  } else {
+    print("Happy analyzing!")
+  }
+}
