4986b291b47b8cf684d502539686537ee0a16cfd
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-03-13 10:18:11 -0400

    Create first_n_days.R

---
+++ 00_scripts/first_n_days.R
@@ -0,0 +1,29 @@
+first_30_days_tbl <- downloads_tbl %>%
+    group_by(package) %>%
+    select(date_time, package) %>%
+    summarise_by_time(
+        .date_var = date_time,
+        .by = "day",
+        value = n()
+    ) %>%
+    mutate(cum_sum = cumsum(value)) %>%
+    mutate(rec_no = row_number()) %>%
+    ungroup() %>%
+    mutate(package = as.factor(package)) %>%
+    filter(rec_no <= 30)
+
+plot_time_series(
+    .data = first_30_days_tbl,
+    .date_var = rec_no,
+    .value = value,
+    .color_var = package,
+    .smooth = FALSE
+)
+
+plot_time_series(
+    .data = first_30_days_tbl,
+    .date_var = rec_no,
+    .value = cum_sum,
+    .color_var = package,
+    .smooth = FALSE
+)
